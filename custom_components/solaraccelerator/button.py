8b64b69a9c2f579d9214c2e4625e2ca39765af3f
aLAN-LDZ <alan.j.com@gmail.com>
2026-01-18 20:45:38 +0000

    fix

--- custom_components/solaraccelerator/button.py
+++ custom_components/solaraccelerator/button.py
@@ -11,7 +11,7 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.device_registry import DeviceEntryType, DeviceInfo
 
 from .const import DOMAIN
-from .sensor import async_send_data
+from .sensor import async_send_data, async_fetch_prices
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -26,16 +26,16 @@ async def async_setup_entry(
     coordinator_data = hass.data[DOMAIN][entry.entry_id]
 
     async_add_entities([
-        SolarAcceleratorForceSendButton(hass, entry, coordinator_data),
+        SolarAcceleratorSyncButton(hass, entry, coordinator_data),
     ])
 
 
-class SolarAcceleratorForceSendButton(ButtonEntity):
-    """Button to force send data to SolarAccelerator."""
+class SolarAcceleratorSyncButton(ButtonEntity):
+    """Button to synchronize data with SolarAccelerator."""
 
     _attr_has_entity_name = True
-    _attr_icon = "mdi:cloud-upload"
-    _attr_translation_key = "force_send"
+    _attr_icon = "mdi:sync"
+    _attr_translation_key = "sync"
 
     def __init__(
         self,
@@ -47,8 +47,8 @@ class SolarAcceleratorForceSendButton(ButtonEntity):
         self.hass = hass
         self.entry = entry
         self.coordinator_data = coordinator_data
-        self._attr_unique_id = f"{entry.entry_id}_force_send"
-        self._attr_name = "Wymuś wysłanie danych"
+        self._attr_unique_id = f"{entry.entry_id}_sync"
+        self._attr_name = "Synchronizuj"
 
     @property
     def device_info(self) -> DeviceInfo:
@@ -63,5 +63,6 @@ class SolarAcceleratorForceSendButton(ButtonEntity):
 
     async def async_press(self) -> None:
         """Handle the button press."""
-        _LOGGER.info("Force sending data to SolarAccelerator")
+        _LOGGER.info("Synchronizing data with SolarAccelerator")
         await async_send_data(self.hass, self.coordinator_data)
+        await async_fetch_prices(self.hass, self.coordinator_data)
